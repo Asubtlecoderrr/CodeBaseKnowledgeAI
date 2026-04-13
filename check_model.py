635ff8e7f4469b9237126de18a66db57a3cac655
Vidhi Doshi <vidhi.doshi@sap.com>
2026-04-13 16:48:59 +0530

    initial commit

--- check_model.py
+++ check_model.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyCkXd4SwMKcYEWjgN97mUiXCquBAvkrahQ")
+genai.configure(api_key="key")
 for m in genai.list_models():
     if "generateContent" in m.supported_generation_methods:
         print(m.name)
